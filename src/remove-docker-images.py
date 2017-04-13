77810bdeee2bba04ce0e92612a007a48d608d4f2
Prateek Nayak <prateek.nayak1708@gmail.com>
2017-04-14 09:23:35 +1000

    Minor modifications to remove docker

--- src/remove-docker-images.py
+++ src/remove-docker-images.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
 
 import docker
-temp_file_location = "/tmp/docker-images"
 save_images = ["aws-sso", "alpine"]
-
-
-##############################################################################
-def write_to_file(text):
-    f = open(temp_file_location, 'w')
-    f.write(text)
-    f.close()
 
 
 ##############################################################################
@@ -27,7 +19,7 @@ def main():
         # sigh !! lets do some crazy shenanigans.
         # all is not lost. Name can be found in the repo tags in the attrs list.
         repo_tag = image.attrs['RepoTags'][0].split(":")[0]
-
+        print("Repo tag: ----------------> {}".format(repo_tag))
         can_delete_image = True
         for image_to_save in save_images:
             if image_to_save in repo_tag:
@@ -37,11 +29,12 @@ def main():
         # checking if this image is aws-sso image (whole point of this script)
         if can_delete_image:
             try:
-                print("Deleting image {} {}".format(repo_tag, "banana"))
+                print("Deleting image {}".format(repo_tag))
                 client.images.remove(image.short_id, force=True, noprune=True)
             except Exception as e:
                 print("Exception raised while deleting image {}".format(repo_tag))
                 print(str(e))
+
 
 if __name__ == '__main__':
     main()
